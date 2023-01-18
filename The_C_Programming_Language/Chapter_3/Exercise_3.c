4c28d55b601e1718f9d1e9cbaa9bf086f5b20569
wangzixing <wangzixinghd@163.com>
2023-01-18 09:25:31 +0800

    Notes 3.6

--- The_C_Programming_Language/Chapter_3/Exercise_3.c
+++ The_C_Programming_Language/Chapter_3/Exercise_3.c
@@ -5,7 +5,6 @@
  * cases like a-b-c and a-z0-9 and -a-z . 
  * Arrange that a leading or trailing - is taken literally.
  */
-
 #include <stdio.h>
 #include <string.h>
 
@@ -21,7 +20,6 @@ int main(void) {
     while ( s[i] ) {
 
         /*  Expand and print the next string in our array s[]  */
-
         expand(result, s[i]);
         printf("Unexpanded: %s\n", s[i]);
         printf("Expanded  : %s\n", result);
@@ -34,7 +32,6 @@ int main(void) {
 
 /*  Copies string s2 to s1, expanding
     ranges such as 'a-z' and '8-3'     */
-
 void expand(char * s1, char * s2) {
     static char upper_alph[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static char lower_alph[27] = "abcdefghijklmnopqrstuvwxyz";
@@ -46,14 +43,12 @@ void expand(char * s1, char * s2) {
 
 
     /*  Loop through characters in s2  */
-
     while ( s2[i] ) {
         switch( s2[i] ) {
             case '-':
                 if ( i == 0 || s2[i+1] == '\0' ) {
 
                     /*  '-' is leading or trailing, so just copy it  */
-
                     s1[j++] = '-';
                     ++i;
                     break;
@@ -65,7 +60,6 @@ void expand(char * s1, char * s2) {
                         so, store pointers to the first and last characters
                         in the range in start and end, respectively. If
                         not, output and error message and skip this range.    */
-
                     if ( (start = strchr(upper_alph, s2[i-1])) &&
                          (end   = strchr(upper_alph, s2[i+1])) )
                         ;
@@ -80,7 +74,6 @@ void expand(char * s1, char * s2) {
                         /*  We have mismatched operands in the range,
                             such as 'a-R', or '3-X', so output an error
                             message, and just copy the range expression.  */
-
                         fprintf(stderr, "EX3_3: Mismatched operands '%c-%c'\n",
                                 s2[i-1], s2[i+1]);
                         s1[j++] = s2[i-1];
@@ -90,7 +83,6 @@ void expand(char * s1, char * s2) {
 
 
                     /*  Expand the range  */
-
                     p = start;
                     while ( p != end ) {
                         s1[j++] = *p;
@@ -111,13 +103,11 @@ void expand(char * s1, char * s2) {
                         a range, so just skip it - the range will
                         be processed in the next iteration of
                         the loop.                                   */
-
                     ++i;
                 }
                 else {
 
                     /*  Just a normal character, so copy it  */
-
                     s1[j++] = s2[i++];
                 }
                 break;
