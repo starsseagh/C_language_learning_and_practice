e24ee1d0e293da89db80633ff5dd87721b292cbd
wangzixing <wangzixinghd@163.com>
2023-02-17 21:16:14 +0800

    video P8

--- 比特-C语言零基础教程/test_2_13/test.c
+++ 比特-C语言零基础教程/test_2_13/test.c
@@ -52,3 +52,43 @@
 //     return 0;
 // }
 
+// int main()
+// {
+//     int arr[10] = {0};
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     int max = arr[0];
+//     for (int i = 1; i < 10; i++)
+//     {
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+        
+//     }
+//     printf("最大的数是%d", max);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     // 打印
+//     // for (int i = 0; i < 10; i++)
+//     // {
+//     //     printf("%d ", arr[i]);
+//     // }
+//     int *p = arr;
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d ", *p);
+//         p++;
+//     }
+    
+//     return 0;
+// }
