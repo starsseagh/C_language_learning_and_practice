c337c67b2d7387f0e3cbf27f0653fbfd99c82017
wangzixing <wangzixinghd@163.com>
2023-02-21 20:29:24 +0800

    video p13-p18

--- 比特-C语言零基础教程/test/test_2_19.c
+++ 比特-C语言零基础教程/test/test_2_19.c
@@ -19,21 +19,43 @@
 // 局部变量的生命周期：进入局部范围生命开始，出局部范围生命结束
 // 全局变量的声明周期：整个程序的生命周期
 
-int main()
-{
-    // 字符串再结尾的位置隐藏了一个“0”结束标志
-    // char arr[] = "hello";
+// int main()
+// {
+//     // 字符串再结尾的位置隐藏了一个“0”结束标志
+//     // char arr[] = "hello";
 
-    char arr1[] = "abc";
-    char arr2[] = {'a', 'b', 'c'};
+//     char arr1[] = "abc";
+//     char arr2[] = {'a', 'b', 'c'};
 
-    // 求字符串的长度
-    printf("%d\n", strlen(arr1));   // 3
-    printf("%d\n", strlen(arr2));   // 未知
+//     // 求字符串的长度
+//     printf("%d\n", strlen(arr1));   // 3
+//     printf("%d\n", strlen(arr2));   // 未知
 
-    printf("%s\n", arr1);
-    printf("%s\n", arr2);   // 打印效果不一样
+//     printf("%s\n", arr1);
+//     printf("%s\n", arr2);   // 打印效果不一样
 
     
+//     return 0;
+// }
+
+// int main()
+// {
+//     printf("%c\n", '\130');     // 8进制130
+//     // X  -- ASCII码值是88
+//     printf("%c\n", '\x30');     // 48是字符'0'
+
+//     printf("%d\n", strlen("c:\test\328\test.c"));   // 长度为14
+
+//     return 0;
+// }
+
+int main()
+{
+    // int a = 10;      // C++注释风格
+    /*
+    int b =0;
+    */
+   // C语言注释风格 - 不支持嵌套注释
+
     return 0;
 }
