478c233f22d8a6f3325eff31f0782dbc557a3d4a
wangzixing <wangzixinghd@163.com>
2023-02-24 21:01:58 +0800

    video p26

--- 比特-C语言零基础教程/test/test_2_23.c
+++ 比特-C语言零基础教程/test/test_2_23.c
@@ -28,13 +28,37 @@ typedef unsigned int u_int;
 // }
 
 // define定义宏
-#define ADD(X, Y)  X+Y
-#define ADD1(X, Y) ((X)+(Y))
+// #define ADD(X, Y)  X+Y
+// #define ADD1(X, Y) ((X)+(Y))
+
+// int main()
+// {
+//     printf("%d\n", 4*ADD(2, 3));
+//     printf("%d\n", 4*ADD1(2, 3));
+
+//     return 0;
+// }
 
 int main()
 {
-    printf("%d\n", 4*ADD(2, 3));
-    printf("%d\n", 4*ADD1(2, 3));
+    int a = 10;     // a在内存中要分配空间的 - 4个字节
+    printf("%p\n", &a);     // %p 专门用来打印地址的
+
+    int* pa = &a;       // pa是用来存放地址的，在C语言中pa叫是指针变量
+    printf("%p\n", pa); 
+    // *说明pa是指针变量
+    // int说明pa执行的对象是int类型的
+
+    *pa = 20;       // * 解引用操作符，通过地址找到a
+    printf("%d\n", a);
+
+    printf("%d\n", sizeof(char*));
+    printf("%d\n", sizeof(short*));
+    printf("%d\n", sizeof(int*));
+    printf("%d\n", sizeof(long*));
+    printf("%d\n", sizeof(long long*));
+    printf("%d\n", sizeof(float*));
+    printf("%d\n", sizeof(double*));
 
     return 0;
 }
