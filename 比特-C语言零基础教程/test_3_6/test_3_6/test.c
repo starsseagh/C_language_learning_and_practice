b04541b37a17df61d767eabd50dd694ab9500bcb
wangzixing <wangzixinghd@163.com>
2023-03-06 21:44:35 +0800

    video p45

---
+++ 比特-C语言零基础教程/test_3_6/test_3_6/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello bit";
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+int main()
+{
+	char arr[] = "hello bit";
+	memset(arr, 'x', 5);
+
+	printf("%s\n", arr);
+
+	return 0;
+}
